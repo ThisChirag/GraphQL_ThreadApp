4a4f96a77b0c527a1e0e440fd7667797d4d3de1b
Chirag Sharma <er.chiragsharma.atemail@gmail.com>
2024-12-07 03:36:35 +0530

    feat(prisma): prisma Setup Done
    Signed-off-by: Chirag Sharma <er.chiragsharma.atemail@gmail.com>

--- thread-app-backend/dist/index.js
+++ thread-app-backend/dist/index.js
@@ -7,6 +7,7 @@ const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const server_1 = require("@apollo/server");
 const { expressMiddleware } = require("@apollo/server/express4");
+const db_1 = require("./db");
 const PORT = 8000;
 async function startServer() {
     const app = (0, express_1.default)();
@@ -16,18 +17,37 @@ async function startServer() {
         hello: String
         say(name: String): String
         }
+
+        type Mutation {
+            createUser(firstName: String!, lastName: String!, email: String!, password: String!): Boolean
+        }
+
         `,
         resolvers: {
             Query: {
                 hello: () => `Hey there, i'm graphql server`,
-                say: (_, { name }) => `Hey ${name}, How are you?`
-            }
-        }
+                say: (_, { name }) => `Hey ${name}, How are you?`,
+            },
+            Mutation: {
+                createUser: async (_, { firstName, lastName, email, password, }) => {
+                    await db_1.prismaClient.user.create({
+                        data: {
+                            email,
+                            firstName,
+                            lastName,
+                            password,
+                            salt: 'random_salt',
+                        },
+                    });
+                    return true;
+                },
+            },
+        },
     });
     app.use((0, cors_1.default)());
     app.use(express_1.default.json());
     await server.start();
-    app.use('/graphql', expressMiddleware(server));
+    app.use("/graphql", expressMiddleware(server));
     app.listen(PORT, () => {
         console.log(`Server is listening on ${PORT} `);
     });
--- thread-app-backend/src/index.ts
+++ thread-app-backend/src/index.ts
@@ -1,40 +1,71 @@
-import express, { query } from 'express';
-import cors from 'cors';
-import { ApolloServer } from '@apollo/server';
-const {expressMiddleware} = require("@apollo/server/express4") // ** need to understand this...
-import axios from 'axios';
+import express, { query } from "express";
+import cors from "cors";
+import { ApolloServer } from "@apollo/server";
+const { expressMiddleware } = require("@apollo/server/express4"); // ** need to understand this...
+import axios from "axios";
+import { prismaClient } from "./db";
 
 const PORT = 8000;
 
-async function startServer(){
-    const app = express();
-    const server = new ApolloServer({
-        typeDefs:`
+async function startServer() {
+  const app = express();
+  const server = new ApolloServer({
+    typeDefs: `
         type Query{
         hello: String
         say(name: String): String
         }
-        `
-        ,
-        resolvers:{
-            Query: {
-                hello: ()=> `Hey there, i'm graphql server`,
-                say: (_, {name}: {name:String}) => `Hey ${name}, How are you?`
-            }
+
+        type Mutation {
+            createUser(firstName: String!, lastName: String!, email: String!, password: String!): Boolean
         }
-    });
 
-    app.use(cors());
-    app.use(express.json());
+        `,
+    resolvers: {
+      Query: {
+        hello: () => `Hey there, i'm graphql server`,
+        say: (_, { name }: { name: string }) => `Hey ${name}, How are you?`,
+      },
+      Mutation: {
+        createUser: async (
+          _,
+          {
+            firstName,
+            lastName,
+            email,
+            password,
+          }: {
+            firstName: string;
+            lastName: string;
+            email: string;
+            password: string;
+          }
+        ) => {
+          await prismaClient.user.create({
+            data: {
+              email,
+              firstName,
+              lastName,
+              password,
+              salt: 'random_salt',
+            },
+          });
+          return true;
+        },
+      },
+    },
+  });
 
-    await server.start();
+  app.use(cors());
+  app.use(express.json());
 
-    app.use('/graphql', expressMiddleware(server));
-    
-    app.listen(PORT, ()=>{
-        console.log(`Server is listening on ${PORT} `);
-    });
+  await server.start();
+
+  app.use("/graphql", expressMiddleware(server));
+
+  app.listen(PORT, () => {
+    console.log(`Server is listening on ${PORT} `);
+  });
 }
 
 startServer();
-
