dc0ba9e4c932870c8022def8a8a478459ff311fd
Chirag Sharma <er.chiragsharma.atemail@gmail.com>
2024-12-06 13:19:47 +0530

    feat(server_component): learnt basic implementation of graphQL
    Signed-off-by: Chirag Sharma <er.chiragsharma.atemail@gmail.com>

--- server/index.js
+++ server/index.js
@@ -1,35 +1,58 @@
 const express = require('express');
-const bodyparser = require('body-parser');
 const cors = require('cors');
 const {expressMiddleware} = require('@apollo/server/express4')
 const {ApolloServer} = require('@apollo/server');
+const {default: axios} = require('axios');
+
 
 async function startServer(){
     const app = express();
     const server = new ApolloServer({
         typeDefs:`
+            type User{
+                id: ID!,
+                name: String!,
+                username: String!,
+                email: String!,
+                phone: String!, 
+                website: String!
+            }
+
             type Todo {
                 id: ID!,
                 title: String!,
                 complete: Boolean
+                userId: ID!
+                user: User
             }
             
             type Query {
                getTodos: [Todo]
+               getAllUsers: [User]
+               getUser(id: ID!): User
             }
         `
         ,
         resolvers:{
+            Todo: {
+                user: async (todo) => (await axios.get(`https://jsonplaceholder.typicode.com/users/${todo.userId}`)).data,
+            },
             Query:{
-                getTodos: ()=> [{id: 1, title: "learning GraphQL", complete: false},
+                getTodos: async ()=> (await axios.get('https://jsonplaceholder.typicode.com/todos')).data,
 
-                ],
-            }
+                getAllUsers: async ()=> (await axios.get('https://jsonplaceholder.typicode.com/users')).data,
+
+                getUser: async (parent, args)=> {
+                    const {id} = args;
+                    (await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)).data;
+                }
+
+            },
         }
     });
 
     app.use(cors());
-    app.use(bodyparser.json());
+    app.use(express.json());
 
     await server.start();
 
@@ -40,3 +63,4 @@ async function startServer(){
 }
 
 startServer();
+
